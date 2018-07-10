80085a31599bace35e0fa5147265e1023822be1f
Étienne Tétreault-Pinard <etienne@plot.ly>
2018-07-10 10:07:41 -0400

    run test-bundle in series
    - to improve log readability
    - to keep cpu levels somewhat constant throughout run

--- tasks/test_bundle.js
+++ tasks/test_bundle.js
@@ -1,16 +1,49 @@
 var path = require('path');
+var exec = require('child_process').exec;
 var glob = require('glob');
+var runSeries = require('run-series');
 
 var constants = require('./util/constants');
-var common = require('./util/common');
-var pathToJasmineBundleTests = path.join(constants.pathToJasmineBundleTests);
+var pathToJasmineBundleTests = constants.pathToJasmineBundleTests;
+var EXIT_CODE = 0;
 
-
+/**
+ * Run all jasmine 'bundle' test in series
+ *
+ * To run specific bundle tests, use
+ *
+ * $ npm run test-jasmine -- --bundleTest=<name-of-suite>
+ */
 glob(pathToJasmineBundleTests + '/*.js', function(err, files) {
-    files.forEach(function(file) {
-        var baseName = path.basename(file);
-        var cmd = 'npm run test-jasmine -- --bundleTest=' + baseName;
+    var tasks = files.map(function(file) {
+        return function(cb) {
+            var cmd = [
+                'karma', 'start',
+                path.join(constants.pathToRoot, 'test', 'jasmine', 'karma.conf.js'),
+                '--bundleTest=' + path.basename(file),
+                '--nowatch'
+            ].join(' ');
 
-        common.execCmd(cmd);
+            console.log('Running: ' + cmd);
+
+            exec(cmd, function(err) {
+                cb(null, err);
+            }).stdout.pipe(process.stdout);
+        };
+    });
+
+    runSeries(tasks, function(err, results) {
+        if(err) throw err;
+
+        console.log('\ntest-bundle summary:\n')
+
+        results.forEach(function(r) {
+            if(r) {
+                EXIT_CODE = 1;
+                console.warn(r.cmd + ' failed');
+            }
+        });
+
+        process.exit(EXIT_CODE);
     });
 });
